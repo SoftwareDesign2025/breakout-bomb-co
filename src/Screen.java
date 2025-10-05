76efa1507ed395af31147b832c46f6a26c1335b4
gavincollins <gavincollins@macbookair.lan>
2025-10-05 18:22:38 -0400

    Updated Screen to have slider and slider is not fully done needs the physics and when the ball htis it

--- src/Screen.java
+++ src/Screen.java
@@ -1,46 +1,45 @@
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class Screen {
 
     private Group root;          
-    private Rectangle background; 
     private Text scoreboard;     
+    private Slider slider;
 
     public Screen() {
-       
         root = new Group();
 
-        // Also if we don't like the color we can change these 
-        background = new Rectangle(0, 0, 800, 600); 
+        
+        javafx.scene.shape.Rectangle background = new javafx.scene.shape.Rectangle(0, 0, 800, 600);
         background.setFill(Color.WHITE);
-
-       
         root.getChildren().add(background);
 
-      
+        
         scoreboard = new Text(10, 20, "Score: 0  Lives: 3");
         scoreboard.setFill(Color.BLACK);
         scoreboard.setFont(new Font(18));
-
-       
         root.getChildren().add(scoreboard);
+
+        
+        slider = new Slider(360); 
+        root.getChildren().add(slider.getNode());
     }
 
-    
     public Group getRoot() {
         return root;
     }
 
-   
+    public Slider getSlider() {
+        return slider;
+    }
+
     public void displayScoreBoard(int score, int lives) {
         scoreboard.setText("Score: " + score + "  Lives: " + lives);
     }
 
-    // Murph call this and the gameWinScreen() in the game loop 
     public void gameOverScreen() {
         Text gameOver = new Text(300, 300, "GAME OVER");
         gameOver.setFill(Color.RED);
@@ -48,22 +47,12 @@ public class Screen {
         root.getChildren().add(gameOver);
     }
 
-   
     public void gameWinScreen() {
         Text win = new Text(300, 300, "YOU WIN!");
         win.setFill(Color.GREEN);
         win.setFont(new Font(36));
         root.getChildren().add(win);
     }
-
-    // Will add these when they are ready
-    public void sliderOnScreen() {
-    	
-    }
-    public void ballOnScreen() {
-    	
-    }
-    public void bricksOnScreen() {
-    	
-    }
+    public void ballOnScreen() { } 
+    public void bricksOnScreen() { }
 }
