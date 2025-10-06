95087c79230d193a1bf5c65c414296a7ea751299
alennemann9 <lennemannaudie@gmail.com>
2025-10-06 11:52:25 -0400

    Move Ball

--- src/GameLoop.java
+++ src/GameLoop.java
@@ -1,14 +1,11 @@
 import javafx.animation.AnimationTimer;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Shape;
-import javafx.scene.shape.Sphere;
 
 public class GameLoop extends AnimationTimer {
-	private Sphere ball;
-	private Rectangle slider;
+	private Ball ball;
+	private Slider slider;
 	private Screen screen;
 	
-	public GameLoop(Sphere ball, Rectangle slider, Screen screen) {
+	public GameLoop(Ball ball, Slider slider, Screen screen) {
 		this.ball = ball;
 		this.slider = slider;
 		this.screen = screen;
@@ -16,6 +13,9 @@ public class GameLoop extends AnimationTimer {
 	
 	@Override
 	public void handle(long now) {
+		
+		ball.updateBallLocation();
+		
 		
 	}
 }
