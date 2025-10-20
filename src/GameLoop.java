8b2fe19a1bf998a5228d5f839e1068cef2cd0f06
alennemann9 <lennemannaudie@gmail.com>
2025-10-20 11:02:08 -0400

    Fixed GameLoop

--- src/GameLoop.java
+++ src/GameLoop.java
@@ -5,14 +5,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.List;
-
 
 public class GameLoop {
 	private Ball ball;
 	private Screen screen;
 	private Slider slider;
-	private PowerUp powerUp;
+	private ArrayList<PowerUp> powerUpList;
 	private int lives = 3;
 	private int points = 0;
 	private int highScore;
@@ -21,16 +19,15 @@ public class GameLoop {
     private final double RESET_X_DIRECTION = 0.2;
     private  final double RESET_Y_DIRECTION = 2;
     private boolean gameOver = false;
-    private final List<PowerUp> activePowerUps = new ArrayList<>();
-
   
 	
-	public GameLoop(Ball ball, Slider slider, Screen screen, PowerUp powerUp) {
+	public GameLoop(Ball ball, Slider slider, Screen screen) {
         this.ball = ball;
         this.slider = slider;
         this.screen = screen;
         this.highScore = getHighScore();
-        this.powerUp = powerUp;
+       
+        
 	}
 	
 	public void handleKeyInput(KeyCode code) {
@@ -45,22 +42,10 @@ public class GameLoop {
 			ball.updateBallLocation();
 			slider.checkSliderCollision(ball);
 			screen.checkBallToWall(ball);
-			int gained = screen.checkBrickCollisions(ball);
-			if (gained > 0) {
-			    points += gained;
-
-			    // Spawn chance happens only when a brick was just broken.
-			    double bx = ball.getBall().getCenterX();
-			    double by = ball.getBall().getCenterY();
-
-			    PowerUp spawned = powerUp.maybeDropPowerUp(bx, by); // uses your spawner instance
-			    if (spawned != null) {
-			        screen.getRoot().getChildren().add(spawned.getNode());
-			        activePowerUps.add(spawned);
-			    }
+			ArrayList<Brick> contactList = screen.checkBrickCollisions(ball);
+			for (Brick contact: contactList) {
+				points += 1;
 			}
-
-
 			if (screen.ballOutOfBounds(ball)) {
 				resetBall();
 			}
@@ -79,35 +64,6 @@ public class GameLoop {
 				gameOverLogic();
 				screen.gameWinScreen();
 			}
-			// Update all falling power-ups and clean up any that fall off-screen
-			for (int i = activePowerUps.size() - 1; i >= 0; i--) {
-			    PowerUp pu = activePowerUps.get(i);
-			    pu.update_position();
-
-			    // If NOT picked up yet, allow off-screen removal
-			    if (!pu.isactivated() && pu.getNode().getBoundsInParent().getMinY() > 600) {
-			        screen.getRoot().getChildren().remove(pu.getNode());
-			        activePowerUps.remove(i);
-			        continue;
-			    }
-			}
-
-			// After they’ve fallen this frame, check for paddle pickups
-			slider.checkPowerUpCollision(activePowerUps, screen);
-
-			// Drive timers for active effects and remove when done
-			for (int i = activePowerUps.size() - 1; i >= 0; i--) {
-			    PowerUp pu = activePowerUps.get(i);
-
-			    if (pu instanceof BiggerSlider) {
-			        BiggerSlider bs = (BiggerSlider) pu;
-			        bs.tick();                   // decrement the per-powerup countdown
-			        if (bs.isPowerUpOver()) {
-			            activePowerUps.remove(i);  // effect already reverted in stopPowerUp()
-			        }
-			    }
-			}
-
 		}
 		
 	}
@@ -136,7 +92,7 @@ public class GameLoop {
 	}
 	
 	private int getHighScore() {
-		try (Scanner in = new Scanner(new File("/Users/alennemann/git/breakout-bomb-co/src/HighScore.txt"))) {
+		try (Scanner in = new Scanner(new File("HighScore.txt"))) {
 		    highScore = in.nextInt();
 		} catch (IOException e) {
 		    highScore = 0;
@@ -146,7 +102,7 @@ public class GameLoop {
 
 	
 	public void setHighScore() {
-	    try (PrintWriter out = new PrintWriter("/Users/alennemann/git/breakout-bomb-co/src/HighScore.txt")) {
+	    try (PrintWriter out = new PrintWriter("HighScore.txt")) {
 	        out.println(points);
 	    } catch (IOException e) {
 	        e.printStackTrace();
