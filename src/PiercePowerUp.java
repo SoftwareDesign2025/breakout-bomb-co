e7da8359236ce0000b07ad369de13ee669e89dac
bfarmer <bfarmer@10.113.57.7>
2025-10-21 22:04:28 -0400

    pierce

--- src/PiercePowerUp.java
+++ src/PiercePowerUp.java
@@ -1,4 +1,30 @@
 
 class PiercePowerUp extends PowerUp{
 
+	private Ball ball;
+	private boolean finished = false;
+	
+	PierceUse(double x, double y){
+		super(x,y);
+	}
+	
+	void setBall(Ball b) {
+		this.ball = b
+		}
+	
+	void startEffect(Slider slider) {
+		if (ball != null) ball.addPierceUse();
+		finished = true;
+	}
+
+	
+	//helper functions
+	boolean isFinished() {
+		return finished;
+		
+	}
+
+
+
+
 }
