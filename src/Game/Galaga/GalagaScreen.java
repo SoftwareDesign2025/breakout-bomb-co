b53932242e3003ac98f1b27c9324c218e82aff30
jcollins38 <jcollins38@elon.edu>
2025-11-03 18:59:40 -0500

    Added star background

--- src/Game/Galaga/GalagaScreen.java
+++ src/Game/Galaga/GalagaScreen.java
@@ -5,6 +5,9 @@ import Objects.Galaga.Ship;
 import Objects.Galaga.GalagaEnemies;
 import Objects.Galaga.GalagaEnemy;
 import Objects.SideMover;
+import javafx.animation.AnimationTimer;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
@@ -12,6 +15,7 @@ import javafx.scene.text.Text;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class GalagaScreen extends Screen {
     private Ship ship;
@@ -19,16 +23,88 @@ public class GalagaScreen extends Screen {
     private List<GalagaEnemy> enemyList;
     private GalagaEnemies enemies;
 
+    // Canvas and variables for star background
+    private Canvas starCanvas;
+    private List<Star> stars = new ArrayList<>();
+    private Random rand = new Random();
+
     public GalagaScreen() {
         super();
+
+        // Black background rectangle
         Rectangle bg = new Rectangle(0, 0, 800, 600);
         bg.setFill(Color.BLACK);
         root.getChildren().add(0, bg);
+
+        // Star canvas (drawn behind gameplay elements)
+        starCanvas = new Canvas(800, 600);
+        root.getChildren().add(1, starCanvas);
+
+        // Create and start star animation
+        initStars(200);
+        animateStars();
+
+        // Original Galaga setup
         enemyList = new ArrayList<>();
         galagaLevelMaker = new GalagaLevelMaker(root, enemyList);
         enemies = new GalagaEnemies(enemyList);
-
         loadLevel(1);
+    }
+
+    // Simple class to store each star's data
+    private class Star {
+        double x, y, speed, size;
+        Color color;
+        Star(double x, double y, double speed, double size, Color color) {
+            this.x = x;
+            this.y = y;
+            this.speed = speed;
+            this.size = size;
+            this.color = color;
+        }
+    }
+
+    // Create a list of randomly placed colorful stars
+    private void initStars(int count) {
+        for (int i = 0; i < count; i++) {
+            stars.add(new Star(
+                    rand.nextDouble() * 800,
+                    rand.nextDouble() * 600,
+                    1 + rand.nextDouble() * 3, // vertical speed
+                    1 + rand.nextDouble() * 3, // size
+                    getRandomColor()            // random color
+            ));
+        }
+    }
+
+    // Return a random star color from a set of bright colors
+    private Color getRandomColor() {
+        Color[] colors = {
+                Color.WHITE, Color.CYAN, Color.YELLOW, Color.LIGHTPINK,
+                Color.LIGHTBLUE, Color.ORANGE, Color.VIOLET, Color.LIME
+        };
+        return colors[rand.nextInt(colors.length)];
+    }
+
+    // Continuously animate stars falling to create space effect
+    private void animateStars() {
+        GraphicsContext gc = starCanvas.getGraphicsContext2D();
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                gc.clearRect(0, 0, 800, 600);
+                for (Star s : stars) {
+                    s.y += s.speed;
+                    if (s.y > 600) {
+                        s.y = 0;
+                        s.x = rand.nextDouble() * 800;
+                        s.color = getRandomColor();
+                    }
+                    gc.setFill(s.color);
+                    gc.fillOval(s.x, s.y, s.size, s.size);
+                }
+            }
+        }.start();
     }
 
     public ArrayList<SideMover> getSideMoverList() { return galagaLevelMaker.getSideMoverList(); }
@@ -49,6 +125,7 @@ public class GalagaScreen extends Screen {
     public GalagaLevelMaker getGalagaLevelMaker() {
         return galagaLevelMaker;
     }
+
     @Override
     public void gameOverScreen() {
         Text over = new Text(300, 300, "GAME OVER LOSER");
