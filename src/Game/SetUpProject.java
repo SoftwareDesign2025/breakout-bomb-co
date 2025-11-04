32a3a5756c2679d0ca39bf8aef5faf53fed4cae1
jcollins38 <jcollins38@elon.edu>
2025-11-04 18:45:04 -0500

    Added stars to the main menu

--- src/Game/SetUpProject.java
+++ src/Game/SetUpProject.java
@@ -1,8 +1,7 @@
 /*
 Authors:
 Murph Lennemann
-
- */
+*/
 
 package Game;
 
@@ -10,17 +9,23 @@ import Game.Breakout.BreakoutLoop;
 import Game.Breakout.BreakoutScreen;
 import Game.Galaga.GalagaLoop;
 import Game.Galaga.GalagaScreen;
+import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class SetUpProject extends Application {
 
@@ -33,13 +38,11 @@ public class SetUpProject extends Application {
     private Stage stage;
     private GameLoop gameLoop;
 
-    /**
-     * Authors:
-     * @param stage the primary stage for this application, onto which
-     * the application scene can be set.
-     * Applications may create other stages, if needed, but they will not be
-     * primary stages.
-     */
+    // Star background fields
+    private Canvas starCanvas;
+    private List<double[]> stars = new ArrayList<>();
+    private Random rand = new Random();
+
     @Override
     public void start(Stage stage) {
         this.stage = stage;
@@ -47,23 +50,19 @@ public class SetUpProject extends Application {
         stage.setTitle("BOMB CO Arcade");
         stage.show();
     }
-    // Starting at the method allows you to see the menu and has the buttons that allow the game to start, with the methods
-    // below. You can add more buttons or change anything in this method as long as you start from this method
 
-    /**
-     * Authors: Murph
-     */
     private void showMainMenu() {
         Group menuRoot = new Group();
 
+        // Add star canvas behind everything
+        starCanvas = new Canvas(GAME_WIDTH, GAME_HEIGHT);
+        menuRoot.getChildren().add(starCanvas);
+        initStars(150);
+        animateStars();
 
-        // Background for the breakout and galaga button, can change color and size here
         Rectangle breakoutBackground = createBackground(300);
-
         Rectangle galagaBackground = createBackground(400);
 
-
-        // Adds the top text and centers and lets you change font, size and color
         Text title = new Text("BOMB CO ARCADE");
         title.setFill(Color.BLUE);
         title.setFont(Font.font("Impact", 96));
@@ -71,8 +70,6 @@ public class SetUpProject extends Application {
         title.setY(150);
         menuRoot.getChildren().add(title);
 
-
-        // Actual text for the button, adds the on mouse clicked which sends to the methods below to start the games
         Text breakoutText = new Text("PLAY BREAKOUT");
         handleText(breakoutText, 300);
         breakoutBackground.setOnMouseClicked(e -> startBreakout());
@@ -89,11 +86,36 @@ public class SetUpProject extends Application {
         stage.setScene(menuScene);
     }
 
-    /**
-     * Authors: Murph
-     * @param text
-     * @param yNum
-     */
+    private void initStars(int count) {
+        for (int i = 0; i < count; i++) {
+            stars.add(new double[]{
+                    rand.nextDouble() * GAME_WIDTH,
+                    rand.nextDouble() * GAME_HEIGHT,
+                    1 + rand.nextDouble() * 3,
+                    1 + rand.nextDouble() * 3
+            });
+        }
+    }
+
+    private void animateStars() {
+        GraphicsContext gc = starCanvas.getGraphicsContext2D();
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                gc.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+                gc.setFill(Color.DEEPSKYBLUE);
+                for (double[] star : stars) {
+                    star[1] += star[2];
+                    if (star[1] > GAME_HEIGHT) {
+                        star[1] = 0;
+                        star[0] = rand.nextDouble() * GAME_WIDTH;
+                    }
+                    gc.fillOval(star[0], star[1], star[3], star[3]);
+                }
+            }
+        }.start();
+    }
+
     public void handleText(Text text, int yNum) {
         text.setFont(Font.font("Impact", 36));
         text.setFill(Color.BLACK);
@@ -101,11 +123,6 @@ public class SetUpProject extends Application {
         text.setY(yNum + 45);
     }
 
-    /**
-     * Authors: Murph
-     * @param yInt
-     * @return
-     */
     public Rectangle createBackground(int yInt) {
         Rectangle background = new Rectangle(300, 60, Color.BLUE);
         background.setX(GAME_WIDTH / 2.0 - 150);
@@ -113,45 +130,31 @@ public class SetUpProject extends Application {
         return background;
     }
 
-    /**
-     * Authors: Murph
-     */
     private void startBreakout() {
         BreakoutScreen breakoutScreen = new BreakoutScreen(LEVEL);
         gameLoop = new BreakoutLoop(breakoutScreen);
         startGame(breakoutScreen);
     }
 
-    /**
-     * Authors: Murph
-     */
     private void startGalaga() {
         GalagaScreen galagaScreen = new GalagaScreen(LEVEL);
         gameLoop = new GalagaLoop(galagaScreen);
         startGame(galagaScreen);
-        }
+    }
 
-    /**
-     * Authors: Murph
-     * @param screen
-     */
-        public void startGame(Screen screen) {
-            Scene gameScene = new Scene(screen.getRoot(), GAME_WIDTH, GAME_HEIGHT);
-            gameScene.setOnKeyPressed(e -> gameLoop.keyPressed(e.getCode()));
-            gameScene.setOnKeyReleased(e -> gameLoop.keyReleased(e.getCode()));
-            gameScene.setOnMouseClicked(e -> gameLoop.startMoving());
-            stage.setScene(gameScene);
-            KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> gameLoop.step());
-            Timeline animation = new Timeline();
-            animation.setCycleCount(Timeline.INDEFINITE);
-            animation.getKeyFrames().add(frame);
-            animation.play();
-        }
+    public void startGame(Screen screen) {
+        Scene gameScene = new Scene(screen.getRoot(), GAME_WIDTH, GAME_HEIGHT);
+        gameScene.setOnKeyPressed(e -> gameLoop.keyPressed(e.getCode()));
+        gameScene.setOnKeyReleased(e -> gameLoop.keyReleased(e.getCode()));
+        gameScene.setOnMouseClicked(e -> gameLoop.startMoving());
+        stage.setScene(gameScene);
+        KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> gameLoop.step());
+        Timeline animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(frame);
+        animation.play();
+    }
 
-
-    /**
-     * Authors: Murph
-     */
     public static void runGame() {
         launch();
     }
